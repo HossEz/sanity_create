942336b7682fe9f8eda331265eb4b91fbad6d47a
HossEz <41141267+HossEz@users.noreply.github.com>
2023-04-14 11:42:30 +0200

    ferdigg

--- frontend/src/components/FrontPage.js
+++ frontend/src/components/FrontPage.js
@@ -1,3 +1,29 @@
+import { useEffect, useState } from "react"
+import { fetchAllShows } from "../utils/sanity/showServices"
+import { Link } from "react-router-dom"
+
 export default function FrontPage(){
-    return <h1>Frontpage</h1>
+    const [shows, setShows] = useState(null)
+
+    async function getShows() {
+        const data = await fetchAllShows()
+        setShows(data)
+    }
+
+    useEffect(() =>{
+        getShows()
+    }, [])
+
+    console.log(shows)
+
+    return (
+        <>
+        <h1>Frontpage</h1>
+        <ul>
+            {shows?.map((show, index) => <li key={index}>
+                <Link to={show._id}>{show.title}</Link>
+            </li>)}
+        </ul>
+        </>
+    )
 }
